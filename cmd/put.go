5bf319d3e46e3c6a47d43925afd8882f0a405c8e
kota.nomura <nom3nan@gmail.com>
2020-02-02 17:22:54 +0900

    :recycle: Rename variables in put command

--- cmd/put.go
+++ cmd/put.go
@@ -14,9 +14,9 @@ var putCmd = &cobra.Command{
 	Use:   "put",
 	Short: "Put environment variables",
 	Args: func(cmd *cobra.Command, args []string) error {
-		if dryrun {
-			diff = "all"
-		} else if !util.SliceContains(diffs, diff) {
+		if putDryrun {
+			putDiff = "all"
+		} else if !util.SliceContains(putDiffs, putDiff) {
 			return fmt.Errorf("hoge")
 		}
 		return nil
@@ -24,7 +24,7 @@ var putCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {		
 		// load dotenv file
 		envs := env.New()
-		for _, file := range envFiles {
+		for _, file := range putEnvFiles {
 			err := envs.LoadDotenv(file)
 			if err != nil {
 				cmd.PrintErrln(err)
@@ -33,7 +33,7 @@ var putCmd = &cobra.Command{
 		}
 
 		// load env variables
-		err := envs.LoadEnvVars(strings.Join(envVars, "\n"))
+		err := envs.LoadEnvVars(strings.Join(putEnvVars, "\n"))
 		if err != nil {
 			cmd.PrintErrln(err)
 			os.Exit(1)
@@ -42,7 +42,7 @@ var putCmd = &cobra.Command{
 		// put env variables
 		client := paramstore.New(getPath(), getEmptyPattern())
 		var oldenvs *env.Env
-		if !dryrun {
+		if !putDryrun {
 			oldenvs, err = client.PutEnvs(envs)
 			if err != nil {
 				cmd.PrintErrln(err)
@@ -56,25 +56,25 @@ var putCmd = &cobra.Command{
 			}
 		}
 		
-		if diff != "no" {
-			env.PrintDiff(oldenvs, envs, diff)			
+		if putDiff != "no" {
+			env.PrintDiff(oldenvs, envs, putDiff)
 		}
 	},
 }
 var (
-	envVars []string
-	envFiles []string
-	dryrun bool
-	diff string
+	putEnvVars []string
+	putEnvFiles []string
+	putDryrun bool
+	putDiff string
 
-	diffs = []string{"no", "all", "key"}
+	putDiffs = []string{"no", "all", "key"}
 )
 
 func init() {
-	putCmd.Flags().StringSliceVarP(&envVars, "env", "e", []string{}, "environment variable (e.g. '-e ENV_VAR=value')")
-	putCmd.Flags().StringSliceVarP(&envFiles, "env-file", "f", []string{}, "dotenv file")
-	putCmd.Flags().BoolVar(&dryrun, "dry-run", false, "")
-	putCmd.Flags().StringVar(&diff, "diff", "no", "")
+	putCmd.Flags().StringSliceVarP(&putEnvVars, "env", "e", []string{}, "environment variable (e.g. '-e ENV_VAR=value')")
+	putCmd.Flags().StringSliceVarP(&putEnvFiles, "env-file", "f", []string{}, "dotenv file")
+	putCmd.Flags().BoolVar(&putDryrun, "dry-run", false, "")
+	putCmd.Flags().StringVar(&putDiff, "diff", "no", "")
 	putCmd.Flags().Lookup("diff").NoOptDefVal = "all"
 
 	rootCmd.AddCommand(putCmd)
