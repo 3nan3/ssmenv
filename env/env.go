f8f151a7f3374377f94830323048d0c23be22343
kota.nomura <nom3nan@gmail.com>
2020-02-02 15:10:37 +0900

    :+1: Support sorting by env name

--- env/env.go
+++ env/env.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"strings"
 	"bytes"
+	"sort"
 )
 
 type Env struct {
@@ -18,10 +19,11 @@ func New() *Env {
 }
 
 func PrintDiff(oldenv *Env, newenv *Env, diff string) {
-	for newk, newv := range newenv.envs {
-		oldv := oldenv.GetEnv(newk)
+	for _, name := range newenv.sortedName() {
+		newv := newenv.GetEnv(name)
+		oldv := oldenv.GetEnv(name)
 		if oldv == "" || newv != oldv {
-			fmt.Printf("- key: %s\n", newk)
+			fmt.Printf("- key: %s\n", name)
 			if diff == "all" {
 				fmt.Printf("  old_value: %s\n  new_value: %s\n", oldv, newv)
 			}
@@ -52,8 +54,8 @@ func (env *Env) ApplyEnv() {
 }
 
 func (env *Env) print(io io.Writer) error {
-	for name, value := range env.envs {
-		escapedValue, err := escapeValue(value); if err != nil {
+	for _, name := range env.sortedName() {
+		escapedValue, err := escapeValue(env.GetEnv(name)); if err != nil {
 			return err
 		}
 
@@ -62,6 +64,17 @@ func (env *Env) print(io io.Writer) error {
 		}
 	}
 	return nil
+}
+
+func (env *Env) sortedName() []string {
+	names := []string{}
+	for name, _ := range env.envs {
+		names = append(names, name)
+	}
+	sort.Slice(names, func(i, j int) bool {
+		return strings.Compare(names[i], names[j]) < 0
+	})
+	return names
 }
 
 func escapeValue(value string) (string, error) {
