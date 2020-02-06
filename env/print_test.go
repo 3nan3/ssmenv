b6a0dbebf582781baf9fcfbad5347056033da0d9
kota.nomura <nom3nan@gmail.com>
2020-02-06 23:20:44 +0900

    :green_heart: Add env print test

---
+++ env/print_test.go
@@ -0,0 +1,51 @@
+package env
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToEnvValue(t *testing.T) {
+	convertions := map[string]string {
+		"value":   `value`,
+		"'value'": `'value'`,
+		`"value"`: `\"value\"`,
+		`va"lue`:  `va\"lue`,
+		"va lue":  `"va lue"`,
+		"va\nlue": "\"va\nlue\"",
+		`va\nlue`: `va\nlue`,
+		"va=lue":  `va=lue`,
+		"": "",
+`{
+  "a": "v",
+  "b": "ABC\nDEF\n"
+}`: 
+`"{
+  \"a\": \"v\",
+  \"b\": \"ABC\nDEF\n\"
+}"`,
+	}
+
+	for value, expected := range convertions {
+		actual, err := toEnvValue(&value)
+		if assert.Nil(t, err) {
+			assert.Equal(t, expected, actual, "Origin value: %s", value)
+		}
+	}
+
+	// nil -> ""
+	actual, err := toEnvValue(nil)
+	if assert.Nil(t, err) {
+		assert.Equal(t, "", actual, "Origin value: %s", nil)
+	}
+}
+
+func TestToDiffValue(t *testing.T) {
+	value := `{
+  "a": "v",
+  "b": "ABC\nDEF\n"
+}`
+	assert.Equal(t, value, toDiffValue(&value))
+	assert.Equal(t, "<undefined>", toDiffValue(nil))
+}
+
